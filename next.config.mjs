b9761ea3a67f5bed01b864bc69c9ca5e42138158
254guru <otienooluda@gmail.com>
2026-03-31 20:29:12 +0300

    add webpack alias

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [],
     formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config) => {
+    config.resolve.alias['.prisma/client'] = path.resolve(
+      __dirname,
+      'node_modules/.prisma/client',
+    );
+    return config;
   },
 };
 
